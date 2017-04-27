860c9c6f452afd1f6245836b9afe53f8cf1a5da9
rituchakrawarty <rituchakrawarty@gmail.com>
2017-04-27 13:22:34 -0500

    Update setalarm.py

--- setalarm.py
+++ setalarm.py
@@ -36,7 +36,7 @@ def makeWebhookResult(req):
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "Welcome to Ritz webhook " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
