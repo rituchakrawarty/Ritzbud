cdf60dbe4c8d8bbe3f4b9ba7761652c25e637ecd
rituchakrawarty <rituchakrawarty@gmail.com>
2017-04-27 11:26:42 -0500

    Update setalarm.py

--- setalarm.py
+++ setalarm.py
@@ -1,11 +1,16 @@
 from __future__ import print_function
 import httplib2
 import os
+import urllib
+import json
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from flask import Flask
+from flask import request
+from flask import make_response
 
 import datetime
 
@@ -21,68 +26,52 @@ SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
+# Flask app should start in global layout
+app = Flask(__name__)
 
-def get_credentials():
-    """Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    res = makeWebhookResult(req)
 
-def main():
-    """Shows basic usage of the Google Calendar API.
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
-    Creates a Google Calendar API service object and outputs a list of the next
-    10 events on the user's calendar.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "alarm.set":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    eventdate = parameters.get("date")
+    eventtime = parameters.get("time")
 
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-  
-    event = {
-       'summary': 'My Alarm',
-       'location': 'home sweet home',
-       'description': 'A chance to hear more about Google\'s developer products.',
-       'start':{'dateTime': '2017-04-28T09:00:00-07:00','timeZone': 'America/Los_Angeles',},
-       'end':{'dateTime': '2017-04-28T17:00:00-07:00','timeZone': 'America/Los_Angeles',},
-       'recurrence': ['RRULE:FREQ=DAILY;COUNT=2'],
-       'attendees': [{'email': 'lpage@example.com'},{'email': 'sbrin@example.com'},],
-       'reminders': {
-       'useDefault': False,
-       'overrides': [
-       {'method': 'email', 'minutes': 24 * 60},
-       {'method': 'popup', 'minutes': 10},
-       ],
-       },
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+    speech = "The event has been scheduled on " + str(eventdate) + " at time " + str(eventtime) + " . IS there anything else I can do?"
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-setalarm"
     }
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created:')
-
-
 if __name__ == '__main__':
-    main()
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
+
